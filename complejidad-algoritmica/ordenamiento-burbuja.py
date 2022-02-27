7fbd0edfabfb1f6b65b2509b950531f21b3029e1
Saúl Sáyago <sayagoadd@gmail.com>
2022-02-26 21:32:42 -0600

    update

--- complejidad-algoritmica/ordenamiento-burbuja.py
+++ complejidad-algoritmica/ordenamiento-burbuja.py
@@ -1,11 +1,11 @@
 import random
 
-# O(n) * O(n) = O(n**2)
+# O(n) * O(n) = O(n * n) = O(n**2)
 def ordenamiento_burbuja(lista):
     n = len(lista)
 
-    for i in range(n): # O(n)
-        for j in range(0, n - i - 1): # O(n)
+    for i in range(n):
+        for j in range(0, n - i - 1):
 
             if lista[j] > lista[j + 1]:
                 lista[j], lista[j + 1] = lista[j + 1], lista[j]
