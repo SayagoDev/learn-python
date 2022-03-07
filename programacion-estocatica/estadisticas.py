8c58faeb952155de687ca1921c1d83887f23c59d
Saúl Sáyago <sayagoadd@gmail.com>
2022-03-06 22:04:01 -0600

    función de Varinza y Desviación Estándar

--- programacion-estocatica/estadisticas.py
+++ programacion-estocatica/estadisticas.py
@@ -5,9 +5,27 @@ def media(X):
     return sum(X) / len(X)
 
 
-if __name__ == '__main__':
-    X = [random.randint(1, 21) for i in range(20)]
+def varianza(X):
     mu = media(X)
 
-    print(X)
-    print(mu)
+    acumulador = 0
+    for x in X:
+        acumulador += (x - mu)**2
+
+    return acumulador / len(X)
+
+
+def desviacion_estandar(X):
+    return varianza(X)**.5
+
+
+if __name__ == '__main__':
+    X = [random.randint(9, 12) for i in range(20)]
+    mu = media(X)
+    Var = varianza(X)
+    sigma = desviacion_estandar(X)
+
+    print(f'Arreglo X: {X}')
+    print(f'Media = {mu}')
+    print(f'Varianza = {Var}')
+    print(f'Desviación estándar = {sigma}')
