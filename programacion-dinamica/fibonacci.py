7afb3e852256c87f610db23a596e02233427b19e
Saúl Sáyago <sayagoadd@gmail.com>
2022-03-19 19:01:12 -0600

    solucionando bug

--- programacion-dinamica/fibonacci.py
+++ programacion-dinamica/fibonacci.py
@@ -1,14 +1,18 @@
 import sys
 
 def fibonacci_recursivo(n):
-    if n == 0 or n == 1:
+    if n == 0:
+        return 0
+    if n == 1:
         return 1
 
     return fibonacci_recursivo(n - 1) + fibonacci_recursivo(n - 2)
 
 
 def fibonacci_dinamico(n, memo = {}):
-    if n == 0 or n == 1:
+    if n == 0:
+        return 0
+    if n == 1:
         return 1
 
     try:
@@ -17,7 +21,7 @@ def fibonacci_dinamico(n, memo = {}):
         resultado = fibonacci_dinamico(n - 1, memo) + fibonacci_dinamico(n - 2, memo)
         memo[n] = resultado
 
-        return resultado
+        return memo[n]
 
 
 if __name__ == '__main__':
